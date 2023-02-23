276eaac31f669aaf273573466cbfa1b1821f3e0b
Tran Phi Truong <phinzin2@gmail.com>
2023-02-23 09:52:43 +0700

    avoid duplicate

--- library-management/src/model/ApplicationAction.java
+++ library-management/src/model/ApplicationAction.java
@@ -3,25 +3,25 @@ package model;
 import java.util.List;
 
 public class ApplicationAction extends Action {
+    Action[] actions;
+
     public ApplicationAction(String message, List<CommonObject> objs) {
         super(message, objs);
         attributes.add(new Attribute("function"));
+        actions = new Action[]{
+                new AddAction(Constants.PLEASE_SELECT_DOC_TYPE + "\n" + Constants.ADD_DOC_MENU, objs),
+                new DeleteAction(Constants.ID_TO_DELETE, objs),
+                new DisplayAction(Constants.DISPLAY_ALL_DOCS, objs),
+                new ExitAction(Constants.EXIT, objs),
+                new DefaultAction(Constants.INCORRECT_OPTIONS_MSG, objs)
+        };
     }
 
 
     @Override
     public void doAction() {
-
         int selectedMenuOptions = Integer.parseInt(this.attributes.get(0).getValue().toString());
-        Action[] actions =
-                new Action[]{
-                        new AddAction(Constants.PLEASE_SELECT_DOC_TYPE + "\n" + Constants.ADD_DOC_MENU, objs),
-                        new DeleteAction(Constants.ID_TO_DELETE, objs),
-                        new DisplayAction(Constants.DISPLAY_ALL_DOCS, objs),
-                        new ExitAction(Constants.EXIT, objs),
-                        new DefaultAction(Constants.INCORRECT_OPTIONS_MSG, objs)
-                };
-        actions[selectedMenuOptions-1].execute();
+        actions[selectedMenuOptions - 1].execute();
         execute();
     }
 }
